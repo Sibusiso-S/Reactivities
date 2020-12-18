83c10126f0cb1ead99b9c576612abadc32dba5dd
Sibusiso Sibanyoni <sibusiso.sibanyoni@entelect.co.za>
2020-12-18 16:22:42 +0200

    Initial Commit

---
+++ Domain/Value.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Value
+    {
+        public int ID{get; set;}
+        public string Name{get; set;}
+    }
+}
