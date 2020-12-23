a704e44a02c6216d726c19ab6b91e62fb844ecae
Sibusiso Sibanyoni <sibusiso.sibanyoni@entelect.co.za>
2020-12-23 11:15:44 +0200

    End of Section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -5,7 +5,6 @@ using Application.Activities;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Threading;
 
 namespace API.Controllers
 {
@@ -36,6 +35,19 @@ namespace API.Controllers
         public async Task<ActionResult<Unit>> Create(Create.Command command)
         {
             return await _mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            return await _mediator.Send(new Delete.Command{Id = id});
         }
     }
 
