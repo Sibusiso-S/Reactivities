4690888202203177e5f0e7b03aed87faf2a68064
Sibusiso Sibanyoni <sibusiso.sibanyoni@entelect.co.za>
2021-03-03 17:30:54 +0200

    End of section 15.

--- client-app/src/app/utils/utils.ts
+++ client-app/src/app/utils/utils.ts
@@ -1,13 +1,36 @@
-const combineDateAndTime = (date: Date, time : Date) =>{
-    const timeString = time.getHours() + ':' + time.getMinutes()+":00";
+import { IActivity, IAttendee } from '../models/activity';
+import { IUser } from '../models/user';
 
-    const year = date.getFullYear();
-    const month = date.getMonth() +1;
-    const day = date.getDate();
+const combineDateAndTime = (date: Date, time: Date) => {
+	const timeString = time.getHours() + ':' + time.getMinutes() + ':00';
 
-    const dateString = `${year}-${month}-${day}`;
+	const year = date.getFullYear();
+	const month = date.getMonth() + 1;
+	const day = date.getDate();
 
-    return new Date(dateString + ' ' + timeString);
-}
+	const dateString = `${year}-${month}-${day}`;
 
-export  default combineDateAndTime;
+	return new Date(dateString + ' ' + timeString);
+};
+
+export const setActivityProps = (activity: IActivity, user: IUser) => {
+	activity.date = new Date(activity.date);
+	activity.isGoing = activity.attendees.some(
+		(a) => a.username === user.username
+	);
+	activity.isHost = activity.attendees.some(
+		(a) => a.username === user.username && a.isHost
+	);
+	return activity;
+};
+
+export const createAttendee = (user: IUser): IAttendee => {
+	return {
+		displayName: user.displayName,
+		isHost: false,
+		username: user.username,
+		image: user.image!,
+	};
+};
+
+export default combineDateAndTime;
