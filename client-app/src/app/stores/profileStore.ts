246b8be4ba22f011bdb18c9e73b006e01b491ff5
Sibusiso Sibanyoni <sibusiso.sibanyoni@entelect.co.za>
2021-05-12 17:05:10 +0200

    Restored section 17

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,48 +1,51 @@
 import {
-	action,
-	computed,
-	makeObservable,
-	observable,
-	runInAction,
+  action,
+  computed,
+  makeObservable,
+  observable,
+  runInAction,
 } from 'mobx';
 import agents from '../api/agents';
 import { IProfile } from '../models/profile';
 import { RootStore } from './rootStore';
 
 export default class ProfileStore {
-	rootStore: RootStore;
-	constructor(rootStore: RootStore) {
-		this.rootStore = rootStore;
-		makeObservable(this);
-	}
+  rootStore: RootStore;
+  constructor(rootStore: RootStore) {
+    this.rootStore = rootStore;
+    makeObservable(this);
+  }
 
-	@observable profile: IProfile | null = null;
-	@observable loadingProfile = true;
+  @observable profile: IProfile | null = null;
+  @observable loadingProfile = true;
 
-	@computed get isCurrentUser() {
-		if (this.rootStore.userStore.user && this.profile) {
-			return this.rootStore.userStore.user.username === this.profile.username;
-		} else {
-			return false;
-		}
-	}
+  @computed get isCurrentUser() {
+    if (this.rootStore.userStore.user && this.profile) {
+      return (
+        this.rootStore.userStore.user.username.toLowerCase() ===
+        this.profile.username.toLowerCase()
+      );
+    } else {
+      return false;
+    }
+  }
 
-	@action loadProfile = async (username: string) => {
-		this.loadingProfile = true;
-		console.log('Profile log true');
-		try {
-			const profile = await agents.Profiles.get(username);
-			runInAction(() => {
-				console.log('Profile log false');
-				this.loadingProfile = false;
-				this.profile = profile;
-			});
-		} catch (error) {
-			runInAction(() => {
-				console.log('Profile log false');
-				this.loadingProfile = false;
-			});
-			console.log(error);
-		}
-	};
+  @action loadProfile = async (username: string) => {
+    this.loadingProfile = true;
+    console.log('Profile log true');
+    try {
+      const profile = await agents.Profiles.get(username);
+      runInAction(() => {
+        console.log('Profile log false');
+        this.loadingProfile = false;
+        this.profile = profile;
+      });
+    } catch (error) {
+      runInAction(() => {
+        console.log('Profile log false');
+        this.loadingProfile = false;
+      });
+      console.log(error);
+    }
+  };
 }
